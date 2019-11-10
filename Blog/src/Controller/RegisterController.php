ffcbfdfee90bdff30999389817544829b0e802f4
Margarit Marian Catalin <margarit.catalin97@gmail.com>
2019-11-10 17:06:30 +0200

    Fix register

--- Blog/src/Controller/RegisterController.php
+++ Blog/src/Controller/RegisterController.php
@@ -1,11 +1,13 @@
 <?php
 namespace App\Controller;
+use App\Entity\User;
+use App\Event\UserRegisterEvent;
+use App\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use Symfony\Component\HttpFoundation\Request;
-use App\Entity\User;
-use App\Form\UserType;
 /**
  *
  */
@@ -14,8 +16,11 @@ class RegisterController extends AbstractController
   /**
    * @Route("/register", name="user_register")
   */
-  public function register(UserPasswordEncoderInterface $passwordEncoder, Request $request)
-  {
+  public function register(
+    UserPasswordEncoderInterface $passwordEncoder, 
+    Request $request,
+    EventDispatcherInterface $eventDispatcher
+  ) {
     $user = new User();
     $form = $this->createForm(
       UserType::class,
@@ -32,6 +37,8 @@ class RegisterController extends AbstractController
       $entityManager = $this->getDoctrine()->getManager();
       $entityManager->persist($user);
       $entityManager->flush();
+      $userRegisterEvent = new UserRegisterEvent($user);
+      $eventDispatcher->dispatch(UserRegisterEvent::NAME, $userRegisterEvent);
       return $this->redirectToRoute('post_index');
     }
     return $this->render(
